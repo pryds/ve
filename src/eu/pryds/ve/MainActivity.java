7fe2630b1b6a5a61c6c289c67dcc0ed3b50acfe4
Thomas Pryds <thomas@pryds.eu>
2013-11-10 13:35:12 +0100

    Code formatting

--- src/eu/pryds/ve/MainActivity.java
+++ src/eu/pryds/ve/MainActivity.java
@@ -8,42 +8,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle presses on the action bar items
-	    switch (item.getItemId()) {
-	        case R.id.action_previous:
-	            System.out.println("Previous button pressed");
-	            return true;
-	        case R.id.action_next:
-	        	System.out.println("Next button pressed");
-	            return true;
-	        case R.id.action_settings:
-	            openSettings();
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}
-	
-	private void openSettings() {
-		Intent intent = new Intent(this, SettingsActivity.class);
-		startActivity(intent);
-	}
-
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+        case R.id.action_previous:
+            System.out.println("Previous button pressed");
+            return true;
+        case R.id.action_next:
+            System.out.println("Next button pressed");
+            return true;
+        case R.id.action_settings:
+            openSettings();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    private void openSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+    
 }
--- src/eu/pryds/ve/TranslatableString.java
+++ src/eu/pryds/ve/TranslatableString.java
@@ -4,121 +4,121 @@ import java.util.Hashtable;
 import java.util.Vector;
 
 public class TranslatableString {
-	private String translatorComments;
-	private String extractedComments;
-	private Vector <String> reference;
-	private Vector <String> flags;
-	private String previousContext;
-	private String previousUntranslatedString;
-	private String previousUntranslatedStringPlural;
-	private String context;
-	private String untranslatedString;
-	private String untranslatedStringPlural;
-	private Hashtable<Integer, String> translatedString;
-	
-	public TranslatableString() {
-		translatorComments = "";
-		extractedComments = "";
-		reference = new Vector<String>();
-		flags = new Vector<String>();
-		previousContext = "";
-		previousUntranslatedString = "";
-		previousUntranslatedStringPlural = "";
-		context = "";
-		untranslatedString = "";
-		untranslatedStringPlural = "";
-		translatedString = new Hashtable<Integer, String>();
-	}
-	
-	public String getTranslatorComments() {
-		return translatorComments;
-	}
-	
-	public void addtoTranslatorComments(String postfix) {
-		translatorComments += postfix;
-	}
-	
-	public String getExtractedComments() {
-		return extractedComments;
-	}
-	
-	public void addtoExtractedComments(String postfix) {
-		extractedComments += postfix;
-	}
-	
-	public Vector<String> getReferences() {
-		return reference;
-	}
-	
-	public Vector<String> getFlags() {
-		return flags;
-	}
-	
-	public String getPreviousContext() {
-		return previousContext;
-	}
-	
-	public void addtoPreviousContext(String postfix) {
-		previousContext += postfix;
-	}
-	
-	public String getPreviousUntranslatedString() {
-		return previousUntranslatedString;
-	}
-	
-	public void addtoPreviousUntranslatedString(String postfix) {
-		previousUntranslatedString += postfix;
-	}
-	
-	public String getPreviousUntranslatedStringPlural() {
-		return previousUntranslatedStringPlural;
-	}
-	
-	public void addtoPreviousUntranslatedStringPlural(String postfix) {
-		previousUntranslatedStringPlural += postfix;
-	}
-	
-	public String getContext() {
-		return context;
-	}
-	
-	public void setContext(String context) {
-		this.context = context;
-	}
-	
-	public void addtoContext(String postfix) {
-		context += postfix;
-	}
-	
-	public String getUntranslatedString() {
-		return untranslatedString;
-	}
-	
-	public void setUntranslatedString(String untranslatedString) {
-		this.untranslatedString = untranslatedString;
-	}
-	
-	public void addtoUntranslatedString(String postfix) {
-		untranslatedString += postfix;
-	}
-	
-	public String getUntranslatedStringPlural() {
-		return untranslatedStringPlural;
-	}
-	
-	public void setUntranslatedStringPlural(String untranslatedStringPlural) {
-		this.untranslatedStringPlural = untranslatedStringPlural;
-	}
-	
-	public void addtoUntranslatedStringPlural(String postfix) {
-		untranslatedStringPlural += postfix;
-	}
-	
-	public Hashtable<Integer, String> getTranslatedString() {
-		return translatedString;
-	}
-	
-	public void resetTranslatedString() {
-		translatedString = new Hashtable<Integer, String>();
-	}
+    private String translatorComments;
+    private String extractedComments;
+    private Vector<String> reference;
+    private Vector<String> flags;
+    private String previousContext;
+    private String previousUntranslatedString;
+    private String previousUntranslatedStringPlural;
+    private String context;
+    private String untranslatedString;
+    private String untranslatedStringPlural;
+    private Hashtable<Integer, String> translatedString;
+    
+    public TranslatableString() {
+        translatorComments = "";
+        extractedComments = "";
+        reference = new Vector<String>();
+        flags = new Vector<String>();
+        previousContext = "";
+        previousUntranslatedString = "";
+        previousUntranslatedStringPlural = "";
+        context = "";
+        untranslatedString = "";
+        untranslatedStringPlural = "";
+        translatedString = new Hashtable<Integer, String>();
+    }
+    
+    public String getTranslatorComments() {
+        return translatorComments;
+    }
+    
+    public void addtoTranslatorComments(String postfix) {
+        translatorComments += postfix;
+    }
+    
+    public String getExtractedComments() {
+        return extractedComments;
+    }
+    
+    public void addtoExtractedComments(String postfix) {
+        extractedComments += postfix;
+    }
+    
+    public Vector<String> getReferences() {
+        return reference;
+    }
+    
+    public Vector<String> getFlags() {
+        return flags;
+    }
+    
+    public String getPreviousContext() {
+        return previousContext;
+    }
+    
+    public void addtoPreviousContext(String postfix) {
+        previousContext += postfix;
+    }
+    
+    public String getPreviousUntranslatedString() {
+        return previousUntranslatedString;
+    }
+    
+    public void addtoPreviousUntranslatedString(String postfix) {
+        previousUntranslatedString += postfix;
+    }
+    
+    public String getPreviousUntranslatedStringPlural() {
+        return previousUntranslatedStringPlural;
+    }
+    
+    public void addtoPreviousUntranslatedStringPlural(String postfix) {
+        previousUntranslatedStringPlural += postfix;
+    }
+    
+    public String getContext() {
+        return context;
+    }
+    
+    public void setContext(String context) {
+        this.context = context;
+    }
+    
+    public void addtoContext(String postfix) {
+        context += postfix;
+    }
+    
+    public String getUntranslatedString() {
+        return untranslatedString;
+    }
+    
+    public void setUntranslatedString(String untranslatedString) {
+        this.untranslatedString = untranslatedString;
+    }
+    
+    public void addtoUntranslatedString(String postfix) {
+        untranslatedString += postfix;
+    }
+    
+    public String getUntranslatedStringPlural() {
+        return untranslatedStringPlural;
+    }
+    
+    public void setUntranslatedStringPlural(String untranslatedStringPlural) {
+        this.untranslatedStringPlural = untranslatedStringPlural;
+    }
+    
+    public void addtoUntranslatedStringPlural(String postfix) {
+        untranslatedStringPlural += postfix;
+    }
+    
+    public Hashtable<Integer, String> getTranslatedString() {
+        return translatedString;
+    }
+    
+    public void resetTranslatedString() {
+        translatedString = new Hashtable<Integer, String>();
+    }
 }
